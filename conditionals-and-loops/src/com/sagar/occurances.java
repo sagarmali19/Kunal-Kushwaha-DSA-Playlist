92b3fc7913ddb3e4aed12085934514e78954fc31
sagarmali19 <sagarmali.work@gmail.com>
2023-11-27 23:53:39 +0530

    Coding for conditionals and loops

---
+++ conditionals-and-loops/src/com/sagar/occurances.java
@@ -0,0 +1,24 @@
+package com.sagar;
+
+import java.util.Scanner;
+
+public class occurances {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number here : ");
+        int number = input.nextInt();
+        System.out.println("Enter number for occurances : ");
+        int num_occur = input.nextInt();
+        int count = 0;
+        int digit;
+        while(number != 0){
+            digit = number % 10;
+            if(digit == num_occur){
+                count++;
+            }
+            number = number / 10;
+        }
+        System.out.println("Occured for : " + count);
+
+    }
+}
